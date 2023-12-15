cbfc124496bef99d285fe50b8db37cf590d5ec2b
l1nes8 <egor1glavatyje@gmail.com>
2023-12-15 19:21:32 +0200

    krok4

--- models/contactsModel.js
+++ models/contactsModel.js
@@ -1,16 +1,4 @@
-const mongoose = require("mongoose");
-const config = require("./config");
 const { model, Schema } = require("mongoose");
-
-mongoose
-  .connect(config.mongoURI)
-  .then(() => {
-    console.log("Database connection successful");
-  })
-  .catch((error) => {
-    console.log(error);
-    process.exit(1);
-  });
 
 const contactSchema = new Schema({
   name: {
--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+const config = require("./models/config");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(config.mongoURI)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
